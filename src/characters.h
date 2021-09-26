1d6183131d265e83328191d59ec657a7aec056de
Joshua <metatron1495@gmail.com>
2021-09-26 15:17:54 -0300

    Correccion de menu completa // Se agrega .gitignore

---
+++ src/characters.h
@@ -0,0 +1,69 @@
+
+
+//Clase Kah
+class kah
+{
+private:
+    int pv;
+    int habilidad;
+    int magia;
+    int melee;
+    int dist;
+
+public:
+    void mover();
+    int atacar();
+    bool muerto();
+};
+
+
+
+kah::~kah()
+{
+}
+
+//Clase Pipper
+class pipper
+{
+private:
+    int pv;
+    int habilidad;
+    int magia;
+    int melee;
+    int dist;
+
+public:
+    pipper(/* args */);
+    ~pipper();
+};
+
+pipper::pipper(/* args */)
+{
+}
+
+pipper::~pipper()
+{
+}
+
+//Clase Solomon
+class solomon
+{
+private:
+    int pv;
+    int habilidad;
+    int magia;
+    int melee;
+    int dist;
+
+public:
+    solomon(/* args */);
+    ~solomon();
+};
+
+solomon::solomon(/* args */)
+{
+}
+
+solomon::~solomon()
+{
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 //Fernández Girado Patricia
-//Rodriguez Molina Emilia
+//Rodriguez Moreno Emilia
 //Frías Hernández Joshua
 
 #include <ncurses.h>
@@ -76,11 +76,6 @@ void menuInicio()
 			select();
 			break;
 
-		case 'J':
-			werase(wmenu);
-			select();
-			break;
-
 		case 'i':
 			wclear(wmenu);
 			box(wmenu, 0, 0);
@@ -111,6 +106,11 @@ void menuInicio()
 			{
 				switch (opcion2)
 				{
+				case 'j':
+					werase(wmenu);
+					select();
+					break;
+
 				case 'c':
 					wclear(wmenu);
 					box(wmenu, 0, 0);
@@ -133,170 +133,10 @@ void menuInicio()
 					mvwprintw(wmenu, 17, 36, "Instrumentalista");
 
 					mvwprintw(wmenu, 19, 9, " [V] Volver ");
-
-					char opcion3;
-					refresh();
-
-					while (opcion3 = wgetch(wmenu))
-					{
-						switch (opcion3)
-						{
-						case 'v':
-							wclear(wmenu);
-							box(wmenu, 0, 0);
-							wattron(wmenu, A_BLINK);
-							mvwprintw(wmenu, 0, 4, " [J] JUGAR ");
-							mvwprintw(wmenu, 0, 46, " [S] SALIR ");
-							wattroff(wmenu, A_BLINK);
-							wattron(wmenu, A_STANDOUT);
-							mvwprintw(wmenu, 0, 24, " INSTRUCCIONES ");
-							wattroff(wmenu, A_STANDOUT);
-
-							mvwprintw(wmenu, 3, 3, "Bienvenido, Contendiente: ");
-							mvwprintw(wmenu, 5, 3, "Tu voluntad ha traído grandes recompensas pues");
-							mvwprintw(wmenu, 6, 3, "has sido seleccionado para representar al");
-							mvwprintw(wmenu, 7, 3, "Kandorak 378-BWZ en el próximo encuentro de");
-							mvwprintw(wmenu, 8, 3, "la Fedreación Instrumentalista y sus derivados.");
-							mvwprintw(wmenu, 10, 3, "Tal honor es otorgado solo a aquellos que sean capaces");
-							mvwprintw(wmenu, 11, 3, "de enferntar sus más grandes miedos en el crisol");
-							mvwprintw(wmenu, 12, 3, "de la batalla pues, de perder, tu Kandorak será ");
-							mvwprintw(wmenu, 13, 3, "borrado del algorítmo de La Gran Semilla.");
-
-							mvwprintw(wmenu, 19, 41, " [C] CONTINUAR ");
-
-							char opcion4;
-							refresh();
-
-							while (opcion4 = wgetch(wmenu))
-							{
-								switch (opcion4)
-								{
-								case 'c':
-									wclear(wmenu);
-									box(wmenu, 0, 0);
-									wattron(wmenu, A_BLINK);
-									mvwprintw(wmenu, 0, 4, " [J] JUGAR ");
-									mvwprintw(wmenu, 0, 46, " [S] SALIR ");
-									wattroff(wmenu, A_BLINK);
-									wattron(wmenu, A_STANDOUT);
-									mvwprintw(wmenu, 0, 24, " INSTRUCCIONES ");
-									wattroff(wmenu, A_STANDOUT);
-
-									mvwprintw(wmenu, 3, 3, "Como La Gran Semilla decretó, tendrás la ");
-									mvwprintw(wmenu, 4, 3, "de elegir tu propio Galo (Avatar) con");
-									mvwprintw(wmenu, 5, 3, "características que te llevarán a la victoria... ");
-									mvwprintw(wmenu, 6, 3, "o a la derrota.");
-									mvwprintw(wmenu, 10, 5, "QUE NUESTRA ESENCIA ELEMENTAL PERDURE POR LA NOCHE");
-									mvwprintw(wmenu, 11, 20, "LARGA VIDA AL COMBATE");
-									mvwprintw(wmenu, 12, 16, "LARGA VIDA A LA GRAN SEMILLA");
-									mvwprintw(wmenu, 16, 32, "Honorable Federación");
-									mvwprintw(wmenu, 17, 36, "Instrumentalista");
-									break;
-
-									char opcion5;
-									refresh();
-
-									while (opcion5 = wgetch(wmenu))
-									{
-										switch (opcion5)
-										{
-										case 'j':
-											werase(wmenu);
-											break;
-
-										case 'J':
-											werase(wmenu);
-											break;
-
-										case 's':
-											werase(wmenu);
-											endwin();
-											exit(EXIT_SUCCESS);
-											break;
-
-										case 'S':
-											werase(wmenu);
-											endwin();
-											exit(EXIT_SUCCESS);
-											break;
-
-										default:
-											wclear(wmenu);
-											box(wmenu, 0, 0);
-											wattron(wmenu, A_BLINK);
-											mvwprintw(wmenu, 0, 4, " [J] JUGAR ");
-											mvwprintw(wmenu, 0, 46, " [S] SALIR ");
-											wattroff(wmenu, A_BLINK);
-											wattron(wmenu, A_STANDOUT);
-											mvwprintw(wmenu, 0, 24, " INSTRUCCIONES ");
-											wattroff(wmenu, A_STANDOUT);
-
-											mvwprintw(wmenu, 3, 3, "Como La Gran Semilla decretó, tendrás la ");
-											mvwprintw(wmenu, 4, 3, "de elegir tu propio Galo (Avatar) con");
-											mvwprintw(wmenu, 5, 3, "características que te llevarán a la victoria... ");
-											mvwprintw(wmenu, 6, 3, "o a la derrota.");
-											mvwprintw(wmenu, 10, 5, "QUE NUESTRA ESENCIA ELEMENTAL PERDURE POR LA NOCHE");
-											mvwprintw(wmenu, 11, 20, "LARGA VIDA AL COMBATE");
-											mvwprintw(wmenu, 12, 16, "LARGA VIDA A LA GRAN SEMILLA");
-											mvwprintw(wmenu, 16, 32, "Honorable Federación");
-											mvwprintw(wmenu, 17, 36, "Instrumentalista");
-											break;
-										}
-									}
-
-								default:
-									break;
-								}
-							}
-
-							break;
-
-						default:
-							break;
-						}
-					}
-
 					break;
-
-				case 'C':
-
-					break;
-
-				default:
+				
+				case 'v':
 					wclear(wmenu);
-					box(wmenu, 0, 0);
-					wattron(wmenu, A_BLINK);
-					mvwprintw(wmenu, 0, 4, " [J] JUGAR ");
-					mvwprintw(wmenu, 0, 46, " [S] SALIR ");
-					wattroff(wmenu, A_BLINK);
-					wattron(wmenu, A_STANDOUT);
-					mvwprintw(wmenu, 0, 24, " INSTRUCCIONES ");
-					wattroff(wmenu, A_STANDOUT);
-
-					mvwprintw(wmenu, 3, 3, "Bienvenido, Contendiente: ");
-					mvwprintw(wmenu, 5, 3, "Tu voluntad ha traído grandes recompensas pues");
-					mvwprintw(wmenu, 6, 3, "has sido seleccionado para representar al");
-					mvwprintw(wmenu, 7, 3, "Kandorak 378-BWZ en el próximo encuentro de");
-					mvwprintw(wmenu, 8, 3, "la Fedreación Instrumentalista y sus derivados.");
-					mvwprintw(wmenu, 10, 3, "Tal honor es otorgado solo a aquellos que sean capaces");
-					mvwprintw(wmenu, 11, 3, "de enferntar sus más grandes miedos en el crisol");
-					mvwprintw(wmenu, 12, 3, "de la batalla pues, de perder, tu Kandorak será ");
-					mvwprintw(wmenu, 13, 3, "borrado del algorítmo de La Gran Semilla.");
-
-					wattron(wmenu, A_UNDERLINE);
-					mvwprintw(wmenu, 17, 43, "[C] CONTINUAR");
-					wattroff(wmenu, A_UNDERLINE);
-
-					char opcion2;
-					refresh();
-					break;
-				}
-			}
-
-			break;
-
-		case 'I':
-			wclear(wmenu);
 			box(wmenu, 0, 0);
 			wattron(wmenu, A_BLINK);
 			mvwprintw(wmenu, 0, 4, " [J] JUGAR ");
@@ -305,6 +145,31 @@ void menuInicio()
 			wattron(wmenu, A_STANDOUT);
 			mvwprintw(wmenu, 0, 24, " INSTRUCCIONES ");
 			wattroff(wmenu, A_STANDOUT);
+
+			mvwprintw(wmenu, 3, 3, "Bienvenido, Contendiente: ");
+			mvwprintw(wmenu, 5, 3, "Tu voluntad ha traído grandes recompensas pues");
+			mvwprintw(wmenu, 6, 3, "has sido seleccionado para representar al");
+			mvwprintw(wmenu, 7, 3, "Kandorak 378-BWZ en el próximo encuentro de");
+			mvwprintw(wmenu, 8, 3, "la Fedreación Instrumentalista y sus derivados.");
+			mvwprintw(wmenu, 10, 3, "Tal honor es otorgado solo a aquellos que sean capaces");
+			mvwprintw(wmenu, 11, 3, "de enferntar sus más grandes miedos en el crisol");
+			mvwprintw(wmenu, 12, 3, "de la batalla pues, de perder, tu Kandorak será ");
+			mvwprintw(wmenu, 13, 3, "borrado del algorítmo de La Gran Semilla.");
+
+			mvwprintw(wmenu, 19, 41, " [C] CONTINUAR ");
+					break;
+
+				case 's':
+					werase(wmenu);
+					endwin();
+					exit(EXIT_SUCCESS);
+					break;
+
+				default:
+					break;
+				}
+			}
+
 			break;
 
 		case 's':
@@ -313,17 +178,11 @@ void menuInicio()
 			exit(EXIT_SUCCESS);
 			break;
 
-		case 'S':
-			werase(wmenu);
-			endwin();
-			exit(EXIT_SUCCESS);
-			break;
 		default:
 			break;
 		}
 	}
 }
-
 void select()
 {
 	bool char1, char2, char3;
