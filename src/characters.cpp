bbbf47d631d05ca4756c9d6ee4f5ad827923b6c6
Joshua <metatron1495@gmail.com>
2021-09-26 13:15:28 -0300

    Maquetado de objetos y clases

--- src/characters.cpp
+++ src/characters.cpp
@@ -1,1 +1,67 @@
 #include "characters.h"
+
+class kah
+{
+private:
+    int pv;
+    int habilidad;
+    int magia;
+    int melee;
+    int dist;
+
+public:
+    kah(/* args */);
+    ~kah();
+};
+
+kah::kah(/* args */)
+{
+}
+
+kah::~kah()
+{
+}
+
+class pipper
+{
+private:
+    int pv;
+    int habilidad;
+    int magia;
+    int melee;
+    int dist;
+
+public:
+    pipper(/* args */);
+    ~pipper();
+};
+
+pipper::pipper(/* args */)
+{
+}
+
+pipper::~pipper()
+{
+}
+
+class solomon
+{
+private:
+    int pv;
+    int habilidad;
+    int magia;
+    int melee;
+    int dist;
+
+public:
+    solomon(/* args */);
+    ~solomon();
+};
+
+solomon::solomon(/* args */)
+{
+}
+
+solomon::~solomon()
+{
+}
